47a7fe64ef94d234dba0ef31ada95b7080b8be9c
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-09-28 01:19:08 +0300

    added main mediaqueries

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,27 @@
       refs.modal.classList.toggle("is-hidden");
     }
   })();
+
+  // BURGER-MENU
+
+const burger = document.querySelector('.header-burger');
+const burgerFirstLine = document.querySelector('.firstLine');
+const burgerSecondLine = document.querySelector('.secondLine');
+const burgerthirdLine = document.querySelector('.thirdLine');
+const nav = document.querySelector('.nav-bar');
+const navLink = document.querySelectorAll('.pop-up');
+
+burger.addEventListener('click', () => {
+  burgerFirstLine.classList.toggle('rotate-down');
+  burgerthirdLine.classList.toggle('rotate-up');
+  burgerSecondLine.classList.toggle('disable');
+  nav.classList.toggle('active-nav');
+});
+
+[...navLink].forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('active-nav');
+    burgerFirstLine.classList.remove('rotate-down');
+    burgerthirdLine.classList.remove('rotate-up');
+  });
+});
